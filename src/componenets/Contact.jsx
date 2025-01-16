f4e9ae4fc932d642a6aa47fc5720033cfc1a094e
Huzaifa <huzaifaaamir454@gmail.com>
2025-01-16 20:25:35 +0500

    oye hoye

--- src/componenets/Contact.jsx
+++ src/componenets/Contact.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import picture from "../assets/pic2.jpeg";
+import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 
 const Contact = () => {
   return (
-    <div className="text-white">
-      <h1 className="text-center text-white font-extrabold text-2xl ">Get in Touch</h1>
-      
+    <div className="text-white min-h-screen flex flex-col justify-center items-center py-16">
+      <h1 className="text-center text-white text-3xl font-primary mb-8">
+        Get in Touch
+      </h1>
+
+      <div className="flex justify-between items-center w-full max-w-screen-md">
+        <p className="flex items-center">
+          <FaPhoneAlt className="text-[#00FFF5] mr-2 text-3xl" />
+          Phone:{" "}
+          <span className="text-[#00FFF5] font-semibold ml-1">03252208246</span>
+        </p>
+        <div className="flex flex-col items-center">
+          <div className="w-72 h-72 md:w-96 md:h-96 bg-black overflow-hidden rounded-full border-4 border-primary shadow-lg transform transition-transform duration-500 hover:scale-105 hover:shadow-2xl mb-8">
+            <img
+              src={picture}
+              alt="image"
+              className="object-cover rounded-full py-6"
+            />
+          </div>
+        </div>
+        <p className="flex items-center">
+          <FaEnvelope className="text-[#00FFF5] mr-2 text-3xl" />
+          Email:{" "}
+          <span className="text-[#00FFF5] font-semibold ml-1">
+            huzaifaaamir454@gmail.com
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
--- src/componenets/Projects.jsx
+++ src/componenets/Projects.jsx
@@ -1,39 +1,45 @@
 import car from "../assets/project4.png";
 import pro2 from "../assets/project2.png";
 import pro3 from "../assets/project3.png";
+import pro4 from "../assets/pro4.png";
+import pro5 from "../assets/pro5.png";
 
 const Projects = () => {
+  const projectlinks = [ 
+    { url: "https://github.com/yourusername/project2", isLive: false },
+    { url: "https://github.com/yourusername/project3", isLive: false },
+    { url: "https://github.com/huzaifa454/LampApp.git", isLive: false },
+    { url: "https://github.com/yourusername/project5", isLive: false },
+    { url: "https://ductscareflorida.com", isLive: true },
+  ];
+  const projects = [car, pro2, pro3, pro4, pro5];
   return (
-    <div className="flex flex-col min-h-screen text-white font-primary py-32">
-      <div className="flex flex-col items-center text-center mt-8 space-y-4">
-        <h1 className="text-3xl">Projects</h1>
-        <div className="flex flex-col sm:flex-row sm:space-x-4 sm:mt-8 space-y-4 sm:space-y-0 justify-start">
-         
-          <div className="flex flex-col w-[350px] h-[250px] border-2 border-primary rounded-lg p-4 bg-[rgb(34,40,47)] transition-transform duration-300 transform hover:scale-105">
-            <img
-              src={car}
-              alt="Car Project"
-              className="w-full h-[200px] object-cover rounded-md mb-4 transition-transform duration-300 hover:scale-110"
-            />
-          </div>
-
-         
-          <div className="flex flex-col w-[350px] h-[250px] border-2 border-primary rounded-lg p-4 bg-[rgb(34,40,47)] transition-transform duration-300 transform hover:scale-105">
-            <img
-              src={pro2}
-              alt="Project 2"
-              className="w-full h-[200px] object-cover rounded-md mb-4 transition-transform duration-300 hover:scale-110"
-            />
-          </div>
-
-          
-          <div className="flex flex-col w-[350px] h-[250px] border-2 border-primary rounded-lg p-4 bg-[rgb(34,40,47)] transition-transform duration-300 transform hover:scale-105">
-            <img
-              src={pro3}
-              alt="Project 3"
-              className="w-full h-[200px] object-cover rounded-md  transition-transform duration-300 hover:scale-110"
-            />
-          </div>
+    <div className="flex flex-col min-h-screen text-white font-primary py-16">
+      <div className="text-center">
+        <h1 className="text-3xl text-primary">My</h1>
+        <h1 className="text-4xl">Projects</h1>
+        <div className="flex flex-wrap justify-center gap-4 mt-8">
+          {projects.map((project, index) => (
+            <a
+              key={index}
+              href={projectlinks[index].url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="border-2 border-primary rounded-lg p-4 bg-black
+              transform transition-transform duration-300 hover:scale-105"
+              title={
+                projectlinks[index].isLive
+                  ? "View Live Project"
+                  : "View on GitHub"
+              }
+            >
+              <img
+                src={project}
+                alt={`Project ${index + 1}`}
+                className="w-full h-48 object-cover rounded-md transition-transform duration-300 hover:scale-110"
+              />
+            </a>
+          ))}
         </div>
       </div>
     </div>
